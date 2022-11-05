0f0fff78422623b94b1b99587cf57890c9380600
Sardorbek <jf.sardorbek@gmail.com>
2022-11-05 22:48:47 +0500

    first homework attempt

--- src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
+++ src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
@@ -2,5 +2,11 @@ package lang.print.gaps.finalModuleTask;
 
 public class NumberReverter {
     public void revert(int number) {
+        int i = 0;
+        while (number != 0) {
+            i = i * 10 + number % 10;
+            number = number / 10;
+        }
+        System.out.println(i);
     }
 }
